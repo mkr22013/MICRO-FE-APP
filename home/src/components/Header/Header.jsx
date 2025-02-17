3bee7ba5417801b357746e9aef92eacbf1ce35c7
mkr22013 <mkr22013@outlook.com>
2025-02-17 00:06:15 -0800

    updated header details

--- home/src/components/Header/Header.jsx
+++ home/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import MiniCart from "cart/MiniCart";
 import Login from "cart/Login";
 
@@ -6,7 +7,13 @@ function Header() {
   return (
     <div className="p-5 bg-blue-500 text-white text-2xl font-bold rounded-2xl mt-1">
       <div className="flex">
-        <div className="flex-grow text-center">Fidget Spinner Catalog</div>
+        <div className="flex-grow flex text-center">
+          <Link to="/">Fidget Spinner Catalog</Link>
+          <div className="mx-5">|</div>
+          <Link id="cart" to="/cart">
+            Cart
+          </Link>
+        </div>
         <div className="float-end relative">
           <Login />
           <MiniCart />
